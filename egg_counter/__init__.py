5bbab494c34a27dbe2e09b24f9541a7b1b112e79
chunting-linct <chunting.linct@gmail.com>
2025-11-21 14:14:57 +0700

    Setup flask packages

--- egg_counter/__init__.py
+++ egg_counter/__init__.py
@@ -1,2 +1,10 @@
-def add_one(input):
-    return input + 1
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)  # Flask auto-finds static/templates in the package
+
+    # Import routes
+    with app.app_context():
+        from . import routes
+
+    return app
